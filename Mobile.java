534515de101e4006441286fa11e3a10661954ea7
Srinath-K-15 <maduraisrinath@gmail.com>
2026-02-26 17:05:20 +0530

    upload

---
+++ Mobile.java
@@ -0,0 +1,17 @@
+class Mobile
+{
+public static void main(String[]args)
+{
+String mobilename ="iphone 15 pro";
+String mobilebrand ="Apple";
+float screensize = 6.1f;
+int batterycapacity = 3274;
+double price = 134999.99;
+System.out.println(mobilename);
+System.out.println(mobilebrand);
+System.out.println(screensize);
+System.out.println(batterycapacity);
+System.out.println(price);
+
+}
+}
---
+++ Mobile1.java
@@ -0,0 +1,17 @@
+class Mobile1
+{
+public static void main(String[]args)
+{
+String  Mobliename ="Galaxy S24 ultea";
+String mobilebrand ="Samsung";
+float screensize = 5.8f;
+int batterycapacity = 5000;
+double price = 74000;
+
+System.out.println(Mobliename);
+System.out.println(mobilebrand);
+System.out.println(screensize);
+System.out.println(batterycapacity);
+System.out.println(price);
+}
+}
