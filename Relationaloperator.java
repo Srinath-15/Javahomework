925d451373b5055ad80d48d9a4906df9a5604b14
Srinath-K-15 <maduraisrinath@gmail.com>
2026-02-27 21:06:35 +0530

    upload

---
+++ Relationaloperator.java
@@ -0,0 +1,25 @@
+class Relationaloperator
+{
+
+ public static void main(String[]agrs)
+ {
+    int a =10;
+    int b =20;
+	boolean c= a==b;
+	boolean d= a!=b;
+	boolean e= a>b;
+	boolean f= a<b;
+	boolean g= a>=b;
+	boolean h= a<=b;
+	System.out.println("the given condition is "+c);
+	System.out.println("the given condition is "+d);
+	System.out.println("the given condition is "+e);
+	System.out.println("the given condition is "+f);
+	System.out.println("the given condition is "+g);
+	System.out.println("the given condition is "+h);
+
+
+
+
+}
+}
---
+++ Simplecal.java
@@ -0,0 +1,23 @@
+class Simplecal
+{
+public static void main(String[]args)
+{
+  int a = 100;
+  int b = 10;
+  System.out.println("value of a   :"+a);
+   System.out.println("value of b  :"+b);
+   int add=a+b;
+   int sub=a-b;
+   int mul=a*b;
+   int mod=a%b;
+   int div=a/b;
+   System.out.println("addtion of a and b is: "+add);
+   System.out.println("subraction of a and b is : "+sub);
+   System.out.println("multiplication of a and b :"+mul);
+   System.out.println("modulus of a and b : "+mod);
+   System.out.println("divison a by b is : "+div);
+   
+
+}	
+	
+}
